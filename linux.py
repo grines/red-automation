6ddfa5afc283323b29c2424f4d08ebf859bdb2e3
brian <brian@brians-Mac-mini.local>
2020-07-06 15:52:10 -0700

    initial

--- linux.py
+++ linux.py
@@ -13,21 +13,21 @@ def main():
     techniques = runner.AtomicRunner()
 
     ## Privilege Escalation
-    techniques.execute("T1546.004", position=1) # Add command to .bash_profile
-    techniques.execute("T1546.004", position=2) # Add command to .bashrc
-    techniques.execute("T1053.003", position=1) # Cron - Replace crontab with referenced file
-    techniques.execute("T1053.003", position=2) # Cron - Add script to cron folder
-    techniques.execute("T1547.006", position=1) # Linux - Load Kernel Module via insmod
-    techniques.execute("T1574.006", position=1) # Shared Library Injection via /etc/ld.so.preload
-    techniques.execute("T1574.006", position=2) # Shared Library Injection via LD_PRELOAD
-    techniques.execute("T1548.001", position=1) # Make and modify binary from C source
-    techniques.execute("T1548.001", position=2) # Set a SetUID flag on file
-    techniques.execute("T1548.001", position=3) # Set a SetGID flag on file
-    techniques.execute("T1548.003", position=1) # Sudo usage
-    techniques.execute("T1548.003", position=2) # Unlimited sudo cache timeout
-    techniques.execute("T1548.003", position=3) # Disable tty_tickets for sudo caching
-    techniques.execute("T1543.002", position=1) # Create Systemd Service
-    techniques.execute("T1546.005", position=1) # Trap
+    techniques.execute("T1546.004", position=0) # Add command to .bash_profile
+    techniques.execute("T1546.004", position=1) # Add command to .bashrc
+    techniques.execute("T1053.003", position=0) # Cron - Replace crontab with referenced file
+    techniques.execute("T1053.003", position=1) # Cron - Add script to cron folder
+    techniques.execute("T1547.006", position=0) # Linux - Load Kernel Module via insmod
+    techniques.execute("T1574.006", position=0) # Shared Library Injection via /etc/ld.so.preload
+    techniques.execute("T1574.006", position=1) # Shared Library Injection via LD_PRELOAD
+    techniques.execute("T1548.001", position=0) # Make and modify binary from C source
+    techniques.execute("T1548.001", position=1) # Set a SetUID flag on file
+    techniques.execute("T1548.001", position=2) # Set a SetGID flag on file
+    techniques.execute("T1548.003", position=0) # Sudo usage
+    techniques.execute("T1548.003", position=1) # Unlimited sudo cache timeout
+    techniques.execute("T1548.003", position=2) # Disable tty_tickets for sudo caching
+    techniques.execute("T1543.002", position=0) # Create Systemd Service
+    techniques.execute("T1546.005", position=0) # Trap
 
 
     ## Persistence
