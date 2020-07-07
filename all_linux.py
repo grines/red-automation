a2ebf1b5a25dfdca57816144c92881528ca1664b
brian <brian@brians-Mac-mini.local>
2020-07-06 17:10:14 -0700

    test

--- all_linux.py
+++ all_linux.py
@@ -21,7 +21,8 @@ def main():
         for row in csv_reader:
             # row variable is a list that represents a row in csv
             print(row[1])
-            techniques.execute(row[1], position=str(int(row[3] - 1)), dependencies=True, cleanup=True)
+            position = int(float(row[3] - 1))
+            techniques.execute(row[1], position=position, dependencies=True, cleanup=True)
     
 if __name__ == "__main__":
     main()
