dc518d354c4a98cd5aada2b2c11062cece68ad12
brian <brian@brians-Mac-mini.local>
2020-07-06 21:11:11 -0700

    test

--- all_linux.py
+++ all_linux.py
@@ -10,27 +10,24 @@ def main():
     # Instantiate the AtomicRunner class instance.
     techniques = runner.AtomicRunner()
     
-    # Boot or Logon Initialization Scripts: Rc.common
-    #techniques.execute("T1037.004", position=0)
-
-    # open file in read mode
+    # loop through and execute linux ttps
     with open('indexes/linux.csv', 'r') as read_obj:
-        # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
-        # Iterate over each row in the csv using reader object
         for row in csv_reader:
-            # row variable is a list that represents a row in csv
-            print("---")
+            print("--Executing--")
             print(row[1])
             print(row[2])
             print(row[3])
-            print("---")
+            print("-------------")
             pos = int(row[3]) - 1
             print(pos)
             try:
-                techniques.execute(row[1], position=pos, dependencies=True, cleanup=True)
+                output = techniques.execute(row[1], position=pos, dependencies=True, cleanup=True)
             except:
+                print("Error: " + row[1])
                 pass
+            if output:
+                print("Success: " + row[1])
     
 if __name__ == "__main__":
     main()
